a8bc2681fa2fc2dba23b2fb62cfc90786e65bfae
Glen <glen.kasoff@gmail.com>
2022-06-30 17:11:23 -0400

    api playground

--- client/index.js
+++ client/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import Script from './script.js'
 import styles from './stylesheets/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<BrowserRouter><App /></BrowserRouter>)
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>)
