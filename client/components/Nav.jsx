f35c6ebd3913bb70d616e2a238c719b9a4a49b68
Erika Collins <erikasusancollins@gmail.comclear>
2022-06-30 20:37:06 -0600

    routing

---
+++ client/components/Nav.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../stylesheets/styles.css'
+import {Link} from 'react-router-dom'
+
+
+
+
+
+function Nav()  {
+
+    return (
+        <nav>
+            <h1>Logo</h1> 
+            <ul className='nav-links'>
+                <Link to="/login">
+                  <li>Login</li>
+                </Link>
+                <Link to="/signup">
+                  <li>Shop</li>
+                </Link>
+                <Link to="/mytrips">
+                  <li>My Trips</li>
+                </Link>  
+                <Link to="/about">
+                  <li>About</li>
+                </Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
