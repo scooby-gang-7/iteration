016885a24bb8d079ba496ff99f198afd189b39e8
Jessica Balding <jballey@harding.edu>
2022-07-19 14:02:26 -0600

    signup testing initiated

---
+++ cypress/e2e/Signup.cy.js
@@ -0,0 +1,8 @@
+const { cyan } = require("@mui/material/colors");
+const { RowDescriptionMessage } = require("pg-protocol/dist/messages");
+
+describe('Signup', () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env('login_url'))
+    })
+})
