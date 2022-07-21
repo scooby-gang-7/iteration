e290dfc1f805298158f319ba4797611d33fdc1ed
Kyle McCracken <kmccracken150@gmail.com>
2022-07-21 02:25:25 -0400

    add load condition to displaying mytrips

--- client/components/MyTrips.jsx
+++ client/components/MyTrips.jsx
@@ -6,6 +6,7 @@ import Trip from './Trip';
 const MyTrips = (props) => {
   const [upcomingTrips, setUpcomingTrips] = useState([]);
   const [pastTrips, setPastTrips] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   //on loading, fetch request to get all the trips info for the user
   useEffect(() => {
@@ -23,8 +24,10 @@ const MyTrips = (props) => {
         // props.setTripInfo(triplist);
         console.log(triplist);
         handleAllTrips(triplist);
+        setIsLoaded(true);
       })
       .catch((e) => {
+        setIsLoaded(true);
         console.log(e);
       });
   }, []);
@@ -33,100 +36,105 @@ const MyTrips = (props) => {
 
   const handleAllTrips = (tripList) => {
     const today = new Date();
-    const tempUpcomingTrips=[];
-    const tempPastTrips=[];
-    
+    const tempUpcomingTrips = [];
+    const tempPastTrips = [];
+
     // sort trips by date within the past and upcoming arrays
     tripList
-    .sort((a, b) => {
-      if (a.date_start > b.date_start) return 1;
-      else if (a.date_start < b.date_start) return -1;
-      else if (a.date_start === b.date_start) {
-        return a.date_end > b.date_end ? 1 : -1;
-      }
-    })
-    
-    //check the start date. if the start date is today or future, push trip to tempUpcomingTrips, else push trip to tempPastTrips
-    .forEach((trip) => {
-      const tripStart = new Date(`${trip.date_start}`);
-      if (tripStart.getTime() >= today.getTime()) {
-        tempUpcomingTrips.push(trip);
-      } else {
-        tempPastTrips.push(trip);
-      }
-    });
+      .sort((a, b) => {
+        if (a.date_start > b.date_start) return 1;
+        else if (a.date_start < b.date_start) return -1;
+        else if (a.date_start === b.date_start) {
+          return a.date_end > b.date_end ? 1 : -1;
+        }
+      })
+
+      //check the start date. if the start date is today or future, push trip to tempUpcomingTrips, else push trip to tempPastTrips
+      .forEach((trip) => {
+        const tripStart = new Date(`${trip.date_start}`);
+        if (tripStart.getTime() >= today.getTime()) {
+          tempUpcomingTrips.push(trip);
+        } else {
+          tempPastTrips.push(trip);
+        }
+      });
     setPastTrips(tempPastTrips);
     setUpcomingTrips(tempUpcomingTrips);
-    };
-    
-  return (
-    <Container
-      sx={{
-        p: 4,
-        display: 'flex',
-        flexDirection:'column',
-        justifyContent:'space-evenly',
-        alignItems: 'center',
-        textAlign: 'center',
-      }}
-    >
-      <AddTripModal userInfo={props.userInfo} setTripInfo={props.setTripInfo} />
-      <div id='upcomingTrips'>
-        <Typography variant='h3' m={2} mb={1}>
-          Upcoming Trips
-        </Typography>
-        <Stack>
-          {upcomingTrips.length >= 1 ? (
-            upcomingTrips.map((trip) => {
-              return (
-                <Card p={3} key={trip.trip_id}>
-                  <Trip
-                    trip_id={trip.trip_id}
-                    name={trip.trip_name}
-                    destination={trip.destination}
-                    start={trip.date_start}
-                    end={trip.date_end}
-                  />
-                </Card>
-              );
-            })
-          ) : (
-            <Typography>
-              There are no upcoming trips. Time to start planning! Add a trip by
-              clicking the 'Add Trip' button above.
-            </Typography>
-          )}
-        </Stack>
-      </div>
-      <div id='pastTrips'>
-        <Stack>
-          <Typography variant='h3' m={3} mb={1}>
-            Past Trips
+  };
+
+  if (isLoaded) {
+    return (
+      <Container
+        sx={{
+          p: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          textAlign: 'center',
+        }}
+      >
+        <AddTripModal
+          userInfo={props.userInfo}
+          setTripInfo={props.setTripInfo}
+        />
+        <div id='upcomingTrips'>
+          <Typography variant='h3' m={2} mb={1}>
+            Upcoming Trips
           </Typography>
-          {pastTrips.length >= 1 ? (
-            pastTrips.map((trip) => {
-              return (
-                <Card p={3} key={trip.trip_id}>
-                  <Trip
-                    trip_id={trip.trip_id}
-                    name={trip.trip_name}
-                    destination={trip.destination}
-                    start={trip.date_start}
-                    end={trip.date_end}
-                  />
-                </Card>
-              );
-            })
-          ) : (
-            <Typography>
-              Looks like you don't have any trip history. It's time to start
-              planning!
+          <Stack>
+            {upcomingTrips.length >= 1 ? (
+              upcomingTrips.map((trip) => {
+                return (
+                  <Card p={3} key={trip.trip_id}>
+                    <Trip
+                      trip_id={trip.trip_id}
+                      name={trip.trip_name}
+                      destination={trip.destination}
+                      start={trip.date_start}
+                      end={trip.date_end}
+                    />
+                  </Card>
+                );
+              })
+            ) : (
+              <Typography>
+                There are no upcoming trips. Time to start planning! Add a trip
+                by clicking the 'Add Trip' button above.
+              </Typography>
+            )}
+          </Stack>
+        </div>
+        <div id='pastTrips'>
+          <Stack>
+            <Typography variant='h3' m={3} mb={1}>
+              Past Trips
             </Typography>
-          )}
-        </Stack>
-      </div>
-    </Container>
-  );
+            {pastTrips.length >= 1 ? (
+              pastTrips.map((trip) => {
+                return (
+                  <Card p={3} key={trip.trip_id}>
+                    <Trip
+                      trip_id={trip.trip_id}
+                      name={trip.trip_name}
+                      destination={trip.destination}
+                      start={trip.date_start}
+                      end={trip.date_end}
+                    />
+                  </Card>
+                );
+              })
+            ) : (
+              <Typography>
+                Looks like you don't have any trip history. It's time to start
+                planning!
+              </Typography>
+            )}
+          </Stack>
+        </div>
+      </Container>
+    );
+  }
 };
 
 export default MyTrips;
